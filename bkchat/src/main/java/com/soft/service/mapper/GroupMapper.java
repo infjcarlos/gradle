5fdfe6ff3541cb3369e7a4fc4f093d06002b9f9c
Carlos <inf.juan.carlos@gmail.com>
2017-07-22 22:57:29 -0700

    adding relationship with mapper group vs user

--- bkchat/src/main/java/com/soft/service/mapper/GroupMapper.java
+++ bkchat/src/main/java/com/soft/service/mapper/GroupMapper.java
@@ -4,13 +4,15 @@ import com.soft.domain.Group;
 import com.soft.service.dto.GroupDTO;
 import org.mapstruct.Mapper;
 import org.mapstruct.Mapping;
+import org.mapstruct.Mappings;
 
 /**
  * Created by jcarlos on 7/22/2017.
  */
-@Mapper(componentModel = "spring", uses = {})
+@Mapper(componentModel = "spring", uses = {UserMapper.class,})
 public interface GroupMapper extends EntityMapper<GroupDTO, Group>{
-    //@Mapping(source = "group.owner", target = "user_id")
+
+    @Mapping(source = "owner.id", target = "ownerId")
     GroupDTO toDto(Group group);
     Group toEntity(GroupDTO userDTO);
 }
